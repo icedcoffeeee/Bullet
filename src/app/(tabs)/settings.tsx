8d7a9c0d76a7095024f2ef745925369a21f0266d
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-02-20 18:43:21 +0800

    front page

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
+import { useTheme } from "@/lib/theme";
+import { Button, Text, View } from "react-native";
 
 export default function Page() {
+  const toggleTheme = useTheme((state) => state.toggleTheme);
   return (
     <View>
       <Text className="text-primary text-3xl">Settings</Text>
+      <Button title="Toggle Theme" onPress={toggleTheme} />
     </View>
   );
 }
